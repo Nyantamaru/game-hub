c826bbc1e6b2bf9266026b99c616d571d71c413c
Tzuyu <manekete@gmail.com>
2023-04-20 23:58:08 -0600

    Display platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import PlatformIconList from './PlatformIconList';
 import { Game } from '../hooks/useGames';
 import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
 
@@ -12,6 +12,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
