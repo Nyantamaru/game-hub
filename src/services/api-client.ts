bb07231d22ae9d31db6a90564e0da18d680774f1
Tzuyu <manekete@gmail.com>
2023-06-05 21:15:11 -0600

    Fetch game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -14,14 +14,20 @@ const axiosInstance = axios.create({
 });
 
 class APIClient<T> {
-  endPoint: string;
+  endpoint: string;
   constructor(endpoint: string) {
-    this.endPoint = endpoint;
+    this.endpoint = endpoint;
   }
 
   getAll = (config: AxiosRequestConfig) => {
     return axiosInstance
-      .get<FetchResponse<T>>(this.endPoint, config)
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
       .then((res) => res.data);
   };
 }
